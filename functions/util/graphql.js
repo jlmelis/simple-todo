79f2028eee90c6bc0d83ad012674b8fb57fd007e
jasonmelis <jasonmelis@penguin.lxd>
2020-04-07 20:12:59 -0400

    Fix spacing

--- functions/util/graphql.js
+++ functions/util/graphql.js
@@ -19,7 +19,7 @@ export async function query(query, variables = {}) {
     query: query,
     variables: variables,
     fetchPolicy: 'network-only',
-    });
+  });
   
   return (results.data);
 }
